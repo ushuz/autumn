f858e0b5ada21c2ca56d7ef3753afbcb6991882e
ushuz <hszqqq12@gmail.com>
2015-10-29 22:32:21 +0800

    Update module docstring

--- autumn.py
+++ autumn.py
@@ -6,7 +6,6 @@ from __future__ import print_function
 autumn.py
 
 A simple MySQL ORM, provides basic ORM functionalities in a Pythonic way.
-Coordinating SQL and Python semantics is the primary goal.
 """
 
 
